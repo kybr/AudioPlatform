6ce8a28f3983b9867bf85780d6f5e543629da5e1
Karl Yerkes <karl.yerkes@gmail.com>
2018-02-09 20:06:38 -0800

    oops; forgot a file

---
+++ AudioPlatform/Globals.h
@@ -0,0 +1,12 @@
+#ifndef __AP_GLOBAL__
+#define __AP_GLOBAL__
+
+namespace ap {
+
+static unsigned channelCount = 2;
+static float sampleRate = 44100.0f;
+static unsigned blockSize = 512;
+
+}  // namespace ap
+
+#endif
