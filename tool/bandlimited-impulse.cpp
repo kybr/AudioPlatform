3de1fde153bd9491802a0f43ed233a29c2ea21e6
Karl Yerkes <karl.yerkes@gmail.com>
2018-02-23 11:37:27 -0800

    got the levels a little better

--- tool/bandlimited-impulse.cpp
+++ tool/bandlimited-impulse.cpp
@@ -1,46 +1,47 @@
 #include <cmath>  // std::sin, std::pow, M_PI
 #include <iostream>
 
-float mtof(float m) { return 8.175799f * pow(2.0f, m / 12.0f); }
-float ftom(float f) { return 12.0f * log2(f / 8.175799f); }
+double mtof(double m) { return 8.175799 * pow(2.0, m / 12.0); }
+double ftom(double f) { return 12.0 * log2(f / 8.175799); }
 
 int main() {
-  float sampleRate = 44100;
-  float midi = 127;
-  float phase = 0;
+  double sampleRate = 44100;
+  double midi = ftom(22050);
+  double phase = 0;
 
-  while (midi > 0) {
-    float frequency = mtof(midi);
-    // printf("%f ", frequency);
+  unsigned _highestHarmonic = 1;
+
+  while (midi > 0) {  // ftom(0.999999)) {
+    double frequency = mtof(midi);
 
     unsigned highestHarmonic = 1;
     while (highestHarmonic * frequency < sampleRate / 2) highestHarmonic++;
     highestHarmonic--;
 
-    // printf("%u ", highestHarmonic);
+    if (highestHarmonic != _highestHarmonic) {
+    }
 
-    float distance = ftom(sampleRate / 2) - ftom(highestHarmonic * frequency);
-    // printf("%f ", distance);
-
-    distance /= 10;
-    if (distance > 1) distance = 1;
+    //  printf("f:%f ", frequency);
+    //  printf("h:%u ", highestHarmonic);
 
     // for each harmonic
-    float sum = distance * sin(highestHarmonic * phase);
-    for (unsigned h = highestHarmonic - 1; h > 0; h--) sum += sin(h * phase);
-    // float sum = 0.0f;
-    // for (unsigned h = highestHarmonic; h > 0; h--) sum += sin(h * phase);
+    double sum = 0;
+    for (unsigned h = highestHarmonic; h > 0; h--) sum += sin(h * phase);
 
     // divide out by the number of harmonics because they were all amplitude 1
-    sum /= highestHarmonic;
+    sum /= (highestHarmonic + 0.44);
+    //    sum *= gain;
 
     // spit out the result
     printf("%f\n", sum);
+    // printf("s:%f\n", sum);
 
     // move the phase forward
     phase += 2 * M_PI * frequency / sampleRate / 2;
 
     // totally arbitrary; no meaning; just tuned to be about right
-    midi -= 0.001;
+    midi -= 0.0003;
+
+    _highestHarmonic = highestHarmonic;
   }
 }
